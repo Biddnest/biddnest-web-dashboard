aa2574d424d9c37407ff560145b58306d0f02fc0
imdhanashri <dhanashree.mane78@gmail.com>
2021-02-12 13:03:34 +0530

    updated users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->char('name');
+            $table->char('email')->unique();
+            $table->char('gender')->nullable();
+            $table->date('DOB');           
             $table->timestamps();
+            $table->boolean('deleted');
         });
     }
 

012faaba054f2d017ff04cd55ce465d7d3ea74fc
Amith Raji <amith.raji@diginnovators.com>
2021-03-12 16:28:41 +0530

    Troubleshooting bearer issue

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,11 @@ Route::prefix('v1')->group(function () {
     Route::put("/profile/update",[ApiRouter::class, 'updateProfile']);
 
 
-    Route::get('/sliders',[ApiRouter::class,'getAppSliders']);
-    Route::get('/services',[ApiRouter::class,'getServices']);
-    Route::get('/subservices',[ApiRouter::class,'getSubServices']);
-    Route::get('/inventories',[ApiRouter::class,'getInventories']);
-    
+    Route::post('/sliders',[ApiRouter::class,'getAppSliders']);
+    Route::post('/services',[ApiRouter::class,'getServices']);
+    Route::post('/subservices',[ApiRouter::class,'getSubServices']);
+    Route::post('/inventories',[ApiRouter::class,'getInventories']);
+
 });
 
 

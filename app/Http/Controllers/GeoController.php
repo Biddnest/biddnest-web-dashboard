d4fb843c3e47dd58ca6eb0d7f9640a055e7ddd77
imdhanashri <dhanashree.mane78@gmail.com>
2021-03-16 15:29:58 +0530

    distance function changed

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class GeoController extends Controller
 {
-    public function distance($source_lat, $source_lng, $dest_lat, $dest_lng){
+    public static function distance($source_lat, $source_lng, $dest_lat, $dest_lng){
         return 100.00;
     }
 

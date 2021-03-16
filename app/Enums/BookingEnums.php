dce499b76125dc2283c53578ce28e5c74da2e5bd
imdhanashri <dhanashree.mane78@gmail.com>
2021-03-16 20:45:32 +0530

    changes in booking Enums

--- app/Enums/BookingEnums.php
+++ app/Enums/BookingEnums.php
@@ -5,7 +5,7 @@ use http\Env\Request;
 
 
 class BookingEnums{
-    public static $STATUS =["enquiry"=>0, "placed"=>1, "bidding"=>2, "rebidding"=>3, "payment"=>4, "pending_user_confirmation"=>5,  "awaiting_pickup"=>6, "in_transit"=>7, "completed"=>8, "cancelled"=>9];
+    public static $STATUS =["enquiry"=>0, "placed"=>1, "bidding"=>2, "rebidding"=>3, "payment"=>4,  "awaiting_pickup"=>5, "in_transit"=>6, "completed"=>7, "cancelled"=>8];
 
     public static $BOOKING_TYPE=["economic"=>0, "premium"=>1];
 }

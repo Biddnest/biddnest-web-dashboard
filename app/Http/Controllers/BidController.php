fdd09b0cc3668243c7fc4bee1de53e999df30d84
imdhanashri <dhanashree.mane78@gmail.com>
2021-03-19 12:13:34 +0530

    added try catch in bidcontroller

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -25,16 +25,25 @@ class BidController extends Controller
 {
     public static function addvendors($booking_id)
     {
-        $vendorlist = Organization::where(["status"=>CommonEnums::$YES, "deleted"=>CommonEnums::$NO])->get();
+        try {
+            $vendorlist = Organization::where(["status"=>CommonEnums::$YES, "deleted"=>CommonEnums::$NO])->get();
 
-        foreach($vendorlist as $vendor)
-        {
-            $bid = new Bid;
-            $bid->booking_id=$booking_id;
-            $bid->org_id=$vendor['id'];
-            $bid->bid_type=BidEnums::$BID_TYPE['bid'];
-            $bid->status=BidEnums::$STATUS['active'];
-            $bid_result = $bid->save();
+            $update_status = Booking::where("id", $booking_id)->update("status", BookingEnums::$STATUS['biding']);
+
+            foreach($vendorlist as $vendor)
+            {
+                $bid = new Bid;
+                $bid->booking_id=$booking_id;
+                $bid->org_id=$vendor['id'];
+                $bid->bid_type=BidEnums::$BID_TYPE['bid'];
+                $bid->status=BidEnums::$STATUS['active'];
+                $bid_result = $bid->save();
+            }
+
+            return true;
+
+        } catch (\Exception $e) {
+            return [false, "error"=>$e->getMessage()];
         }
 
     }

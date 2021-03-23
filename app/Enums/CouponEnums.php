97a99bbc11384a0188dac60ee5d73198b38742e3
Amith Raji <amith.raji@diginnovators.com>
2021-03-23 16:35:22 +0530

    Merge branch 'dev' of https://github.com/diginnovators/cp-biddnest-web-backend into dev
    # Conflicts:
    #	resources/views/layouts/app.blade.php
    #	resources/views/layouts/includes/app-css.blade.php

--- app/Enums/CouponEnums.php
+++ app/Enums/CouponEnums.php
@@ -8,7 +8,7 @@ class CouponEnums{
     public static $DISCOUNT_TYPE = ["fixed"=>0,"percentage"=>1];
     public static $ZONE_SCOPE = ["all"=>0,"custom"=>1];
     public static $ORGANIZATION_SCOPE = ["all"=>0,"custom"=>1];
-    public static $SCOPE = ["all"=>0,"custom"=>1];
+//    public static $SCOPE = ["all"=>0,"custom"=>1];
     public static $DEDUCTION_SOURCE = ["admin"=>0,"vendor"=>1];
     public static $USER_SCOPE = ["all"=>0,"custom"=>1];
     public static $STATUS = ["active"=>0,"inactive"=>1, "expired"=>2];

9e51799196d5eb3947b293e70fad38697dba50c5
Amith Raji <amith.raji@diginnovators.com>
2021-03-23 16:01:50 +0530

    Merge branch 'dev' of https://github.com/diginnovators/cp-biddnest-web-backend into dev
    # Conflicts:
    #	resources/views/layouts/app.blade.php
    #	resources/views/layouts/includes/app-css.blade.php

---
+++ app/Enums/PaymentEnums.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Copyright (c) 2021. This Project was built and maintained by Diginnovators Private Limited.
+ */
+namespace App\Enums;
+class PaymentEnums{
+
+    public static $STATUS = ["pending"=>0,"failed"=>2, "completed"=>1];
+
+}
+
+
--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -9,8 +9,15 @@ class Coupon extends Model
 {
     use HasFactory;
 
-    public function __construct(){}
+    public function zones(){
+        return $this->hasManyThrough(Zone::class, CouponZone::class,"zone_id","id","id","coupon_id");
+    }
 
-    public static function add(){}
+    public function organizations(){
+        return $this->hasManyThrough(Organization::class, CouponZone::class,"organization_id","id","id","coupon_id");
+    }
 
+    public function users(){
+        return $this->hasManyThrough(User::class, CouponUser::class,"user_id","id","id","coupon_id");
+    }
 }

74085fbf1e658bc9bd1003df4fc250abdf6e9151
imdhanashri <dhanashree.mane78@gmail.com>
2021-04-07 19:03:01 +0530

    craeted get position API

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -486,7 +486,7 @@ class BookingsController extends Controller
                     ->whereNotIn("status", [BidEnums::$STATUS['rejected'], BidEnums::$STATUS['expired']]);
             }])->with('user')->first();
 
-        return Helper::response(true, "Show data successfully", ["booking" => $booking, 'rank'=>self::getposition($request->token_payload->id, $request->public_booking_id)]);
+        return Helper::response(true, "Show data successfully", ["booking" => $booking, "rank"=>self::getposition($request->token_payload->id, $request->public_booking_id)]);
     }
 
     public static function reject($id, $org_id, $vendor_id)

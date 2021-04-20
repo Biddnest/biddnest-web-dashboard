b0bee268a42a124e7e360f090faf68dee376207b
imdhanashri <dhanashree.mane78@gmail.com>
2021-04-20 20:57:02 +0530

    Fixed Zone list page

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::any("/{anything}",function(){
+Route::prefix("v1")->group(function () {
     \App\Helper::response(false, "", [], 500);
 });
 

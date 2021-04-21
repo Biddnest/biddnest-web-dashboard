92c8e5cc69e78f14f0da404ae96bf90c0927ae91
imdhanashri <dhanashree.mane78@gmail.com>
2021-04-21 11:52:23 +0530

    added user add function

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -129,7 +129,7 @@ class BidController extends Controller
                                                 "bid_result_at"=>$complete_time->format("Y-m-d H:i:s")
                                             ]);
 
-            $update_bid_type = Bid::where("booking_id",$book_id)->whereNotIn("status", BidEnums::$STATUS['rejected'])
+            $update_bid_type = Bid::where("booking_id",$book_id)->whereNotIn("status", [BidEnums::$STATUS['rejected']])
                                 ->update([
                                     "bid_type"=>BidEnums::$BID_TYPE['rebid'],
                                     "status"=>BidEnums::$STATUS['active']

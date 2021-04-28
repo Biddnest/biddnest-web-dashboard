0a5b910fff8faa9c667d88d1c28d0a3d17243f4f
imdhanashri <dhanashree.mane78@gmail.com>
2021-04-28 16:26:23 +0530

    zones added for count

--- resources/views/layouts/includes/app-js.blade.php
+++ resources/views/layouts/includes/app-js.blade.php
@@ -1,71 +1,44 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
-
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-crossorigin="anonymous"></script>
-
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-crossorigin="anonymous"></script>
-
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.js" integrity="sha512-bUg5gaqBVaXIJNuebamJ6uex//mjxPk8kljQTdM1SwkNrQD7pjS+PerntUSD+QRWPNJ0tq54/x4zRV8bLrLhZg==" crossorigin="anonymous"></script>
-
-  <script type="text/javascript" src='https://maps.google.com/maps/api/js?sensor=false&libraries=places'></script>
-
 <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.serializeJSON/3.2.1/jquery.serializejson.min.js" integrity="sha512-SdWDXwOhhVS/wWMRlwz3wZu3O5e4lm2/vKK3oD0E5slvGFg/swCYyZmts7+6si8WeJYIUsTrT3KZWWCknSopjg==" crossorigin="anonymous"></script>
-
-<script
-    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
-
+{{--<script--}}
+{{--    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>--}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js" integrity="sha512-eyHL1atYNycXNXZMDndxrDhNAegH2BDWt1TmkXJPoGf1WLlNYt08CSjkqF5lnCRmdm3IrkHid8s2jOUY4NIZVQ==" crossorigin="anonymous"></script>
 <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 <!-- jsdelivr -->
 <script src="https://cdn.jsdelivr.net/npm/@barba/core"></script>
-
-
+<script type="text/javascript" src='https://maps.google.com/maps/api/js?&key={{json_decode(\App\Models\Settings::where('key','google_api_key')->pluck('value'),true)[0]}}&sensor=false&libraries=places'></script>
+<script src="{{ asset('static/js/locationpicker.jquery.js')}}"></script>
 <script src="{{ asset('static/js/jQuery.tagify.min.js')}}"></script>
-
 <script src="{{ asset('static/js/intlTelInput.js') }}"></script>
-
-<script src="{{ asset('static/js/live-order-chart.js') }}"></script>
+{{--<script src="{{ asset('static/js/live-order-chart.js') }}"></script>--}}
 <script src="{{ asset('static/js/revenue-trand-chart.js') }}"></script>
-
-<script src="{{ asset('static/js/userZone.js') }}"></script>
-
-<script src="{{ asset('static/js/timer.js') }}"></script>
+{{--<script src="{{ asset('static/js/userZone.js') }}"></script>--}}
+{{--<script src="{{ asset('static/js/timer.js') }}"></script>--}}
 <!-- <script src="{{ asset('static/js/progress-bar.js') }}"></script> -->
-
-
 <script src="{{ asset('static/js/maps.js') }}"></script>
 <!-- <script src="{{ asset('static/js/select.js') }}"></script> -->
-
-  <script src="{{ asset('static/js/jquery.tagsinput.min.js')}}"></script>
-
-
-  <script src="{{ asset('static/js/froala_editor.min.js')}}"></script>
+<script src="{{ asset('static/js/jquery.tagsinput.min.js')}}"></script>
+<script src="{{ asset('static/js/froala_editor.min.js')}}"></script>
 <script src="{{ asset('static/js/froala_editor.pkgd.min.js')}}"></script>
-
 <!-- Optional JavaScript -->
 <script src="{{ asset('static/js/sidebarCollapse.js') }}"></script>
-
-<script src="{{ asset('static/js/barba.js') }}"></script>
-
+<script  type="module" src="{{ asset('static/js/barba.js') }}"></script>
 <script src="{{ asset('static/js/jquery.logger.js') }}"></script>
-
-<script src="{{ asset('static/js/main.js') }}"></script>
-
+{{--<script src="{{ asset('static/js/main.js') }}"></script>--}}
 <script src="https://unpkg.com/js-logger/src/logger.min.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.15.5/sweetalert2.all.min.js" integrity="sha512-TxryOYMwWBRIlZoSkKW+jZvJ834vF3u8mE0jDeTLEDdPplOVNNZfWm9VFtEuW365BFPLK5CEIF/vaHqmAey8XA==" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -76,13 +49,8 @@ crossorigin="anonymous"></script>
     const API_SEARCH_USERS = '{{route('search_user')}}';
     const API_SEARCH_VENDOR = '{{route('search_vendor')}}';
     const API_SEARCH_ADMIN = '{{route('search_admin')}}';
-
     @if (\Illuminate\Support\Facades\Session::has('redirect'))
     toastr.success("{{\Illuminate\Support\Facades\Session::get('redirect')}}", "Success", {timeOut: 5000})
     @endif
-
 </script>
-
 <script type="module" src="{{ asset('static/js/app/app.js') }}"></script>
-
-

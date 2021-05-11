bd09cfb58cff97fe89412f4580be8e5514025c24
meghathakur23 <meghnathakur484@gmail.com>
2021-05-11 19:26:22 +0530

    minor issues fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -169,8 +169,8 @@
         <div class="col-sm-8 p-0">
             <div class="card p-10 pl-0 pr-0">
                 <div class="d-flex flex-row justify-content-between">
-                    <div class="p-10 card-head right text-left">
-                        <h3 class="f-18 mt-0">Recent Orders</h3>
+                    <div class="p-10 card-head right text-left ml-3">
+                        <h3 class="f-18 mt-0 ">Recent Orders</h3>
                     </div>
                     {{--<div class="p-10 card-head left">
                         <a><i><img src="{{asset('static/images/filter.svg') }}" alt="" srcset=""></i>

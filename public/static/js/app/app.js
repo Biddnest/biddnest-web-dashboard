ec7758618d8ae1812150d068e0625af848c6320e
imdhanashri <dhanashree.mane78@gmail.com>
2021-05-12 11:49:05 +0530

    fixed admin panel point 16

--- public/static/js/app/app.js
+++ public/static/js/app/app.js
@@ -428,7 +428,7 @@ $("body").on('change', ".reply_status", function(event) {
 
 $("body").on('input', ".table-search", function(event) {
     var query = $(this).val();
-    if(query.length > 0 && query.length < 16){
+    if(query.length >= 15 || query.length >= 5){
         redirectTo($(this).data('url')+"?search="+query);
     }
 });

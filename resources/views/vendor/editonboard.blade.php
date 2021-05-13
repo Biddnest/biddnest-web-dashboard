ef908e8dc80cb9964a2862ef102850c0370d3be3
imdhanashri <dhanashree.mane78@gmail.com>
2021-05-13 11:49:42 +0530

    pull code

--- resources/views/vendor/editonboard.blade.php
+++ resources/views/vendor/editonboard.blade.php
@@ -70,7 +70,7 @@
                                                     <div class="file-upload">
                                                         <input type="hidden" class="base-holder" name="image" value="{{$organization->image}}" required />
                                                         <button type="button" class="btn theme-bg white-text my-0" data-action="upload">UPLOAD IMAGE</button>
-                                                        <input type="file" value="{{$organization->image}}" required/>
+                                                        <input type="file" />
                                                     </div>
                                                     <p>Max File size: 1MB</p>
                                                 </div>

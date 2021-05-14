e9b1148855f733ec0213658e46c79d58e04f3203
imdhanashri <dhanashree.mane78@gmail.com>
2021-05-14 08:12:51 +0530

    fixing Functional points inventory update

--- resources/views/categories/createinventories.blade.php
+++ resources/views/categories/createinventories.blade.php
@@ -30,36 +30,31 @@
                     </h3>
                 </div>
                 <div class="" id="">
-                    <div
-                      class="tab-pane fade show active margin-topneg-15"
-                      id="order"
-                      role="tabpanel"
-                      aria-labelledby="new-order-tab"
-                    >
+                    <div class="tab-pane fade show active margin-topneg-15" id="order" role="tabpanel" aria-labelledby="new-order-tab">
                       <!-- form starts -->
-                    <form action="@if(!$inventory){{route('inventories_add')}}@else{{route('inventories_edit')}}@endif" method="@if(!$inventory){{"POST"}}@else{{"PUT"}}@endif" data-next="redirect" data-redirect-type="hard" data-url="{{route('inventories')}}" data-alert="tiny"
+                        <form action="@if(!$inventory){{route('inventories_add')}}@else{{route('inventories_edit')}}@endif" method="@if(!$inventory){{"POST"}}@else{{"PUT"}}@endif" data-next="redirect" data-redirect-type="hard" data-url="{{route('inventories')}}" data-alert="tiny"
                         class="form-new-order pt-4 mt-3" id="newForm" data-parsley-validate >
-                        <div class="d-flex row pt-3">
-                            @if($inventory)
-                                <input type="hidden" name="id" value="{{$inventory->id}}">
-                            @endif
-                            <div class="col-lg-6">
-                                <p class="img-label">Photo</p>
+                            <div class="d-flex row pt-3">
+                                @if($inventory)
+                                    <input type="hidden" name="id" value="{{$inventory->id}}">
+                                @endif
+                                <div class="col-lg-6">
+                                    <p class="img-label">Photo</p>
                                     <div class="upload-section  pt-0" style="padding-left: 20px;">
-                                    <img class="upload-preview"
+                                        <img class="upload-preview"
                                                 src="@if(!$inventory){{asset('static/images/upload-image.svg')}}@else{{$inventory->image}}@endif"
                                                 alt=""
                                             />
-                                    <div class="ml-1">
-                                        <div class="file-upload">
-                                        <input type="hidden" class="base-holder" name="image" value="@if($inventory){{$inventory->image}}@endif" required />
+                                        <div class="ml-1">
+                                            <div class="file-upload">
+                                                <input type="hidden" class="base-holder" name="image" value="@if($inventory){{$inventory->image}}@endif" required />
                                                 <button type="button" class="btn theme-bg white-text my-0" data-action="upload">
                                                     UPLOAD IMAGE
                                                 </button>
-                                            <input type="file" @if(!$inventory) required @endif/>
+                                                <input type="file" @if(!$inventory) required @endif/>
+                                            </div>
+                                            <p class="text-black pl-2">Max File size: 1MB</p>
                                         </div>
-                                        <p class="text-black pl-2">Max File size: 1MB</p>
-                                    </div>
                                     </div>
                                 </div>
 
@@ -83,106 +78,98 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="d-flex row pl-4 pr-4" style="margin-top: 15px;">
-                        <div class="col-lg-6">
-                            <div class="form-input">
-                                <label class="full-name">Item Name</label>
-                                <input
-                                    type="text"
-                                    id="banner_name"
-                                    name="name"
-                                    placeholder="Name"
-                                    value="@if($inventory){{$inventory->name}}@endif"
-                                    class="form-control br-5" required
-                                  />
-                                <span class="error-message"
-                                    >Please enter a valid banner name</span
-                                  >
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="form-input">
-                                <label class="phone-num-lable">Material</label>
-                                <select class="form-control select-box2" name="material[]" multiple required>
-                                    @if($inventory && $inventory->material)
-                                        @foreach(json_decode($inventory->material) as $material)
-                                            <option value="{{$material}}" selected>{{$material}}</option>
-                                        @endforeach
-                                    @endif
-                                </select>
-                                <span class="error-message"
-                                    >Please enter valid Material</span
-                                  >
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="form-input">
-                                <label class="phone-num-lable">Size</label>
-                                <select class="form-control select-box2" name="size[]" multiple required>
-                                    @if($inventory && $inventory->size)
-                                        @foreach(json_decode($inventory->size) as $size)
-                                            <option value="{{$size}}" selected>{{$size}}</option>
-                                        @endforeach
-                                    @endif
-                                </select>
+                            <div class="d-flex row pl-4 pr-4" style="margin-top: 15px;">
+                                <div class="col-lg-6">
+                                    <div class="form-input">
+                                        <label class="full-name">Item Name</label>
+                                        <input
+                                            type="text"
+                                            id="banner_name"
+                                            name="name"
+                                            placeholder="Name"
+                                            value="@if($inventory){{$inventory->name}}@endif"
+                                            class="form-control br-5" required
+                                          />
+                                        <span class="error-message">Please enter a valid banner name</span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-input">
+                                        <label class="phone-num-lable">Material</label>
+                                        <select class="form-control select-box2" name="material[]" multiple required>
+                                            @if($inventory && $inventory->material)
+                                                @foreach(json_decode($inventory->material) as $material)
+                                                    <option value="{{$material}}" selected>{{$material}}</option>
+                                                @endforeach
+                                            @endif
+                                        </select>
+                                        <span class="error-message">Please enter valid Material</span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-input">
+                                        <label class="phone-num-lable">Size</label>
+                                        <select class="form-control select-box2" name="size[]" multiple required>
+                                            @if($inventory && $inventory->size)
+                                                @foreach(json_decode($inventory->size) as $size)
+                                                    <option value="{{$size}}" selected>{{$size}}</option>
+                                                @endforeach
+                                            @endif
+                                        </select>
 
-                                <span class="error-message"
-                                    >Please enter valid Size</span
-                                  >
-                            </div>
-                        </div>
+                                        <span class="error-message">Please enter valid Size</span>
+                                    </div>
+                                </div>
 
-                        <div class="col-lg-6">
-                            <div class="example mt-3">
-                                <label class="f-14">Category Type </label>
-                                <div>
-                                    <select class="form-control br-5" name="category"  required>
-                                        <option value="">--Select--</option>
-                                        @foreach(\App\Enums\InventoryEnums::$CATEGORY as $type)
-                                            <option value="{{$type}}"
-                                                    @if($inventory && ($inventory->category == $type)) selected @endif
-                                            >{{ucfirst(trans($type))}}</option>
-                                        @endforeach
-                                    </select>
+                                <div class="col-lg-6">
+                                    <div class="example mt-3">
+                                        <label class="f-14">Category Type </label>
+                                        <div>
+                                            <select class="form-control br-5" name="category"  required>
+                                                <option value="">--Select--</option>
+                                                @foreach(\App\Enums\InventoryEnums::$CATEGORY as $type)
+                                                    <option value="{{$type}}"
+                                                            @if($inventory && ($inventory->category == $type)) selected @endif
+                                                    >{{ucfirst(trans($type))}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="mt-5">
+                                <div
+                                    class="d-flex justify-content-between flex-row p-10 py-0"
+                                    style="border-top: 1px solid #70707040"
+                                  >
+                                    <div class="w-50">
+                                      <a class="white-text p-10 cancel" href="#"
+                                        ><button
+                                          class="btn theme-br theme-text w-30 white-bg br-5"
+                                        >
+                                          Cancel
+                                        </button></a
+                                      >
+                                    </div>
+                                    <div class="w-50 text-right">
+                                      <a class="white-text p-10"
+                                        ><button
+                                          class="btn theme-bg white-text w-30 br-5"
+                                        >
+                                          Save
+                                        </button></a
+                                      >
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
+                        </form>
                     </div>
-                    <div class="mt-5">
-                        <div
-                            class="d-flex justify-content-between flex-row p-10 py-0"
-                            style="border-top: 1px solid #70707040"
-                          >
-                            <div class="w-50">
-                              <a class="white-text p-10 cancel" href="#"
-                                ><button
-                                  class="btn theme-br theme-text w-30 white-bg br-5"
-                                >
-                                  Cancel
-                                </button></a
-                              >
-                            </div>
-                            <div class="w-50 text-right">
-                              <a class="white-text p-10"
-                                ><button
-                                  class="btn theme-bg white-text w-30 br-5"
-                                >
-                                  Save
-                                </button></a
-                              >
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
 </div>
-            </div>
-          </div>
 
 
 @endsection

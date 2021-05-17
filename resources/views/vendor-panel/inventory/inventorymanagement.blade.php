6488822750e3e03e4598b617f44d7dfb5183dd1f
imdhanashri <dhanashree.mane78@gmail.com>
2021-05-17 10:41:43 +0530

    changes in inventory price delete

--- resources/views/vendor-panel/inventory/inventorymanagement.blade.php
+++ resources/views/vendor-panel/inventory/inventorymanagement.blade.php
@@ -69,7 +69,9 @@
                                         <div class="actions justify-content-between">
 {{--                                            <i><img src="{{asset('static/vendor/images/Icon material-remove-red-eye.svg')}}" alt="" srcset=""></i>--}}
                                             <a href="{{route('vendor.inventory.edit',['id'=>$inventory->id])}}"><i><img src="{{asset('static/vendor/images/Icon material-edit.svg')}}" alt="" srcset=""></i></a>
-                                            <i><img src="{{asset('static/vendor/images/Icon metro-bin.svg')}}" alt="" srcset=""></i>
+                                            <a href="#" class="delete inline-icon-button" data-confirm="Are you sure, you want delete this Inventory's Prices permenently? You won't be able to undo this." data-url="{{route('api.deleteInventoryPrices', ["id"=>$inventory->id])}}">
+                                                <i><img src="{{asset('static/vendor/images/Icon metro-bin.svg')}}" alt="" srcset=""></i>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>

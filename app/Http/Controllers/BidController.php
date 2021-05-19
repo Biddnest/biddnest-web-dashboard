438f8d65d42e708fbd28585994e741415d36d05d
imdhanashri <dhanashree.mane78@gmail.com>
2021-05-19 15:32:02 +0530

    fixed bug

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -219,7 +219,7 @@ class BidController extends Controller
         {
             $inventory_price = new BidInventory;
             $inventory_price->booking_inventory_id = $key['booking_inventory_id'];
-            $inventory_price->bid_id= Booking::where(['public_booking_id'=>$data['public_booking_id']])->pluck('id')[0];
+            $inventory_price->bid_id= Bid::where(['id'=>Booking::where(['public_booking_id'=>$data['public_booking_id']])->pluck('id')[0], 'organization_id'=>$org_id])->pluck('id')[0];
             $inventory_price->amount=$key['amount'];
             $inventory_result = $inventory_price->save();
         }

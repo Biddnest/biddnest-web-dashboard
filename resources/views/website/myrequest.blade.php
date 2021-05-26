25bd51a2d04be623fe7dd9eef2bfd6ae457052d4
imdhanashri <dhanashree.mane78@gmail.com>
2021-05-26 19:55:38 +0530

    tabs url bug fixed

--- resources/views/website/myrequest.blade.php
+++ resources/views/website/myrequest.blade.php
@@ -9,16 +9,16 @@
                     <h3 class="f-18">
                         <ul class="nav nav-tabs pt-10 p-0" id="myTab" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link light-nav-tab p-15" id="new-order-tab" data-toggle="tab" href="{{route('website.my-profile')}}">My Profile</a>
+                                <a class="nav-link light-nav-tab p-15" href="{{route('website.my-profile')}}">My Profile</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link light-nav-tab p-15" id="new-order-tab" data-toggle="tab" href="{{route('my-bookings-enquiries')}}">Enquiries</a>
+                                <a class="nav-link light-nav-tab p-15" href="{{route('my-bookings-enquiries')}}">Enquiries</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link light-nav-tab p-15" id="quotation" data-toggle="tab" href="{{route('my-bookings')}}">Ongoing Booking</a>
+                                <a class="nav-link light-nav-tab p-15" href="{{route('my-bookings')}}">Ongoing Booking</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link light-nav-tab p-15" id="booking-history" href="{{route('order-history')}}">Booking History</a>
+                                <a class="nav-link light-nav-tab p-15" href="{{route('order-history')}}">Booking History</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link light-nav-tab active p-15" id="request-tab" data-toggle="tab" href="#request" role="tab" aria-controls="profile" aria-selected="false">My Requests</a>

66d039f787eca6088d114597ee0be57f0055379c
Amith Raji <amith.raji@diginnovators.com>
2021-06-15 17:17:50 +0530

    Booking enums colors fixed

--- app/Enums/BookingEnums.php
+++ app/Enums/BookingEnums.php
@@ -7,7 +7,7 @@ class BookingEnums{
 
     public static $STATUS =["enquiry"=>0, "placed"=>1, "biding"=>2, "rebiding"=>3, "payment_pending"=>4, "pending_driver_assign"=>5, "awaiting_pickup"=>6, "in_transit"=>7, "completed"=>8, "cancelled"=>9, "bounced"=>10, "hold"=>11];
 
-    public static $COLOR_CODE =["enquiry"=>"#E5F6FC", "placed"=>"#FF5722", "biding"=>"#FEF6E0", "rebiding"=>"#FF5722", "payment_pending"=>"#FF5722", "pending_driver_assign"=>"#FF5722", "awaiting_pickup"=>"#DDF6E9", "in_transit"=>"#FFF1E3", "completed"=>"#A3E7C5", "cancelled"=>"#FDC5C5", "bounced"=>"#FFEFEF"];
+    public static $COLOR_CODE =["enquiry"=>"#3c69a5", "placed"=>"#e6c32d", "biding"=>"#ff8f30", "rebiding"=>"#e27214", "payment_pending"=>"#ff741e", "pending_driver_assign"=>"#ffaa22", "awaiting_pickup"=>"#238150", "in_transit"=>"#238150", "completed"=>"#3d7c5d", "cancelled"=>"#ce3131", "bounced"=>"#ce3131"];
 
     public static $BOOKING_TYPE=["economic"=>0, "premium"=>1];
 

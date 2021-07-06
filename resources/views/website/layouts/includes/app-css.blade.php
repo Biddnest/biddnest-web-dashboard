d2401e55d5fecd5b23c558d55bf3606618ae9a61
Amith Raji <amith.raji@diginnovators.com>
2021-07-06 14:21:22 +0530

    Datepicker fixed in inner pages

--- resources/views/website/layouts/includes/app-css.blade.php
+++ resources/views/website/layouts/includes/app-css.blade.php
@@ -1092,4 +1092,28 @@
 
 }
 
+    .datepicker .datepicker-dropdown .dropdown-menu .datepicker-orient-left .datepicker-orient-top{
+        position: absolute !important;
+        left: 44% !important;
+        top: 100% !important;
+        margin-left: -96px;
+        margin-top: -73px;
+    }
+
+    .datepicker{
+        position: fixed !important;
+        left: 50% !important;
+        margin-left: auto !important;
+        margin-right: auto !important;
+        z-index: 1;
+        top: 50% !important;
+        transform: translate(-50%, -50%);
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+    }
+
+
+    .datepicker.dropdown-menu th, .datepicker.dropdown-menu td{
+        padding: 13px 15px;
+    }
+
 </style>

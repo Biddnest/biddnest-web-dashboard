db2ce4bca7c6dfc63585543fb1ccaffcf220ff18
imdhanashri <dhanashree.mane78@gmail.com>
2021-08-04 18:35:08 +0530

    adding otp verification in admin bidding

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -26,7 +26,8 @@ class ServiceController extends Controller
         $service=new Service;
         $service->name=$name;
         $service->inventory_quantity_type=$inventory_quantity_type;
-        $service->image = Helper::saveFile($imageman->make($image)->resize(256,256)->encode('png', 100),$image_name,"services");
+//        $service->image = Helper::saveFile($imageman->make($image)->resize(256,256)->encode('png', 100),$image_name,"services");
+        $service->image = Helper::saveFile($imageman->make($image)->resize(256,256),$image_name,"services");
         $result= $service->save();
 
         if(!$result)

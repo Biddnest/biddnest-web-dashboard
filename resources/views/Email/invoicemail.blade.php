7b587ed25a41c5b3e20e679938c9c4db948d7804
imdhanashri <dhanashree.mane78@gmail.com>
2021-08-18 16:37:34 +0530

    testing mail sending

--- resources/views/Email/invoicemail.blade.php
+++ resources/views/Email/invoicemail.blade.php
@@ -82,7 +82,7 @@
     </tr>
     <tr>
         <td style="padding: 0px 10px;">
-            <table width="480" align="center" style="margin:0 auto;" cellpadding="0" cellspacing="0">
+            <table width="480" align="center" style="margin:0 auto; margin-top: 20px; margin-bottom: 20px;" cellpadding="0" cellspacing="0">
 
                 <!-- fix for gmail -->
                 <tr>
@@ -117,9 +117,7 @@
                         <table width="100%" cellpadding="0" cellspacing="0">
                             <tr>
                                 <td align="center" colspan="4" style="font: lighter 35px/40px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #4A4A4A; padding: 0px 50px 40px;">
-                                    Order number:
-                                    <br>
-                                    <p style="font: lighter 24px/28px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #4A4A4A; margin:0px;">65005056005</p>
+                                    Order number:  #65005056005
                                 </td>
                             </tr>
                             <tr>
@@ -133,39 +131,6 @@
                                 <td class="no-link" align="center" width="50">
                                 </td>
                             </tr>
-
-                            <!-- eero Pro 6 -->
-                            <tr>
-                                <td class="no-link" align="center" width="50">
-                                </td>
-                                <td align="left" style="font: 400 24px/29px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 0px 0px 40px;">eero 6 Home Wifi System 3-pack
-                                    <br>
-                                    <br>
-                                </td>
-                                <td align="right" valign="top" style="vertical-align: top; font: 400 24px/29px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 0px 0px 40px;">
-                                    $279.00
-                                    <br>
-                                </td>
-                                <td class="no-link" align="center" width="50">
-                                </td>
-                            </tr>
-
-                            <!-- eero 6 2pk CA-->
-
-                            <!--
-                                          <tr>
-                                              <td mc:edit="block_04" class="no-link" align="center" width="50">
-                                              </td>
-                                              <td align="left" style="font: 400 24px/29px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 40px 0px;" mc:edit="block_05">(1) Mixed SKU, Retail, AG-AL-AL, 3PK, USA
-                                              </td>
-                                              <td align="right" valign="top" style="vertical-align: top; font: 400 24px/29px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 40px 0px;" mc:edit="block_55">
-                                                  $279.00
-                                                  <br>
-                                              </td>
-                                              <td mc:edit="block_06" class="no-link" align="center" width="50">
-                                              </td>
-                                          </tr>
-                                           -->
                             <tr>
                                 <td class="no-link" align="center" width="50">
                                 </td>
@@ -193,18 +158,6 @@
                             <tr>
                                 <td class="no-link" align="center" width="50">
                                 </td>
-                                <td align="left" style="vertical-align: top; font: 300 21px/25px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 20px 0px 40px;">Shipping
-                                </td>
-                                <td align="right" valign="top" style="vertical-align: top; font: 300 21px/25px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 20px 0px 40px;">
-                                    $0.00
-                                    <br>
-                                </td>
-                                <td class="no-link" align="center" width="50">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="no-link" align="center" width="50">
-                                </td>
                                 <td align="left" style="font: 400 24px/29px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 40px 0px 60px; border-width: 1px 0px 0px; border-color: #979797; border-style: solid none none none;">Total
                                 </td>
                                 <td align="right" valign="top" style="vertical-align: top; font: 400 24px/29px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 40px 0px 60px; border-width: 1px 0px 0px; border-color: #979797; border-style: solid none none none;">
@@ -218,11 +171,9 @@
                                 <td class="no-link" align="center" width="50">
                                 </td>
                                 <td align="left" colspan="2" bgcolor="#F7F7F7" style="font: lighter 18px/24px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #5D5D5D; padding: 55px 50px; background-color: #F7F7F7;">
-                                    We’re getting your order ready and we’ll email you with the tracking information as soon as it’s out the door. Here’s where it’s headed.
+                                    We’re getting your order ready and we’ll notify you with the tracking information as soon as Driver assigned. Here’s where it’s headed.
                                     <p style="font: 400 24px/30px Roboto, Source Sans Pro, Helvetica, Arial, sans-serif; color: #4A4A4A; margin:0px; padding: 20px 0px 0px">Shipping address:</p>
-                                    Smiles Davis
-                                    <br> 600 Montgomery St
-                                    <br> San Franscisco, CA 94111
+                                    <br>
                                     <p>If you included an eero 6 or eero Pro 6 product on this order, the entire order will ship on or after November 2, 2020.</p>
                                 </td>
                                 <td class="no-link" align="center" width="50">
@@ -243,33 +194,33 @@
                                                 <table align="center" style="margin: 0 auto;" cellpadding="0" cellspacing="0">
                                                     <tr>
                                                         <td class="active" width="18">
-                                                            <a href="http://mail.eero.com/track/click/30727349/www.facebook.com?p=eyJzIjoic0RVNXZtTDllby1xbXVwclJvTE50Rks1QUM0IiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL3d3dy5mYWNlYm9vay5jb21cXFwvZWVyb1xcXC9cIixcImlkXCI6XCI5ZmRjYTBjOTEwMTM0YzJlOTg1MWY2NDM4OGQ2N2Q2MFwiLFwidXJsX2lkc1wiOltcImIwZTY2N2QzYzU5MzlmZGM1ZDFkMDlkMTIxYTA5NmYwNWQ0NGJlMDFcIl19In0" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/6aa58872-0c66-4f81-a7db-162e01ab3248.png" alt="fb" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" height="23"></a>
+                                                            <a href="#" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/6aa58872-0c66-4f81-a7db-162e01ab3248.png" alt="fb" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" height="23"></a>
                                                         </td>
                                                         <td width="30"></td>
                                                         <td class="active" width="18">
-                                                            <a href="http://mail.eero.com/track/click/30727349/twitter.com?p=eyJzIjoiWGxLWG1jNnB4RGZlY3hpQ25UUHdUcWxWRXh3IiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL3R3aXR0ZXIuY29tXFxcL2dldGVlcm9cIixcImlkXCI6XCI5ZmRjYTBjOTEwMTM0YzJlOTg1MWY2NDM4OGQ2N2Q2MFwiLFwidXJsX2lkc1wiOltcIjYwZGVhNjgyMzI3ZjcxMjAzYzRjMWU2YjJiNDEwNzBhM2FmNWEzZGNcIl19In0" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/49de93dd-9322-477c-ba41-fb66587258d6.png" alt="tw" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" height="23"></a>
+                                                            <a href="#" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/49de93dd-9322-477c-ba41-fb66587258d6.png" alt="tw" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" height="23"></a>
                                                         </td>
                                                         <td width="30"></td>
                                                         <td class="active" width="19">
-                                                            <a href="http://mail.eero.com/track/click/30727349/www.instagram.com?p=eyJzIjoiNkdFMlVqN291b041ekxCaHN2TlM3NWd2ejJRIiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL3d3dy5pbnN0YWdyYW0uY29tXFxcL2dldGVlcm9cIixcImlkXCI6XCI5ZmRjYTBjOTEwMTM0YzJlOTg1MWY2NDM4OGQ2N2Q2MFwiLFwidXJsX2lkc1wiOltcImQ0ODdlMDNjNTg5OGMzOWIxNmU3YTkxMDM1YzQ0MmI4ZGI1ODFiYzNcIl19In0" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/b6208fb3-d9cb-42dc-a2b1-a6bc0d84d03b.png" alt="gp" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" height="23"></a>
+                                                            <a href="#" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/b6208fb3-d9cb-42dc-a2b1-a6bc0d84d03b.png" alt="gp" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" height="23"></a>
                                                         </td>
                                                         <td width="30"></td>
                                                         <td class="active" width="16">
-                                                            <a href="http://mail.eero.com/track/click/30727349/www.linkedin.com?p=eyJzIjoiRWlFSnVvaTZUVjFLS0Zha2lIRldWMWV6N1hvIiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL3d3dy5saW5rZWRpbi5jb21cXFwvY29tcGFueVxcXC9lZXJvXCIsXCJpZFwiOlwiOWZkY2EwYzkxMDEzNGMyZTk4NTFmNjQzODhkNjdkNjBcIixcInVybF9pZHNcIjpbXCIzN2Q3NjM0MDlmMzEwNjM4NzNhNThhZjgxMjllZGNkMWJlMmExYjkyXCJdfSJ9" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/572457be-c24a-43e3-9cc7-cbe16a85aba8.png" alt="li" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" width="23"></a>
+                                                            <a href="#" target="_blank" style="text-decoration: none; color: #c4c4c4;"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/572457be-c24a-43e3-9cc7-cbe16a85aba8.png" alt="li" style="vertical-align:top; height: 23px; font: 12px/17px Arial, Helvetica, sans-serif;" width="23"></a>
                                                         </td>
                                                     </tr>
                                                 </table>
                                                 <table width="100%" cellpadding="0" cellspacing="0">
                                                     <tr>
                                                         <td colspan="4" class="no-link" align="center" bgcolor="#001949" style="font: 300 12px/18px Source Sans Pro, Helvetica, Arial, sans-serif; color: #acacac; padding: 40px 0px 30px;">
-                                                            <a href="http://mail.eero.com/track/click/30727349/eero.com?p=eyJzIjoiaHo1TDZxSEZoZ2V6eXp4Z2RUeUd1OVFaRDlRIiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL2Vlcm8uY29tXCIsXCJpZFwiOlwiOWZkY2EwYzkxMDEzNGMyZTk4NTFmNjQzODhkNjdkNjBcIixcInVybF9pZHNcIjpbXCJiOTIxNmNlNDY0Y2MyOGRjZTBhYTE0YjI2YjE1NDkyMGNhODFlMTE1XCJdfSJ9"><img src="https://gallery.mailchimp.com/227964591a0bc1283160a5186/images/cf812ccd-4cd8-46e1-aa5b-10ece5792f48.png" width="63" style="width: 63;"></a>
+                                                            <a href="http://mail.eero.com/track/click/30727349/eero.com?p=eyJzIjoiaHo1TDZxSEZoZ2V6eXp4Z2RUeUd1OVFaRDlRIiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL2Vlcm8uY29tXCIsXCJpZFwiOlwiOWZkY2EwYzkxMDEzNGMyZTk4NTFmNjQzODhkNjdkNjBcIixcInVybF9pZHNcIjpbXCJiOTIxNmNlNDY0Y2MyOGRjZTBhYTE0YjI2YjE1NDkyMGNhODFlMTE1XCJdfSJ9"><img src="https://dashboard-biddnest.dev.diginnovators.com/static/images/logo.png" width="63" style="width: 63;"></a>
                                                         </td>
                                                     </tr>
 
 
                                                     <tr>
                                                         <td colspan="4" class="no-link" align="center" bgcolor="#001949" style="font: 300 12px/18px Source Sans Pro, Helvetica, Arial, sans-serif; color: #FFFFFF; padding: 0px 0px 0px;">
-                                                            <a href="http://mail.eero.com/track/click/30727349/eero.com?p=eyJzIjoieFk1MmZRanF6SUpjWjNVNHZJcEl6TVV2MTlvIiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL2Vlcm8uY29tXFxcL2xlZ2FsXFxcL3Rvc1wiLFwiaWRcIjpcIjlmZGNhMGM5MTAxMzRjMmU5ODUxZjY0Mzg4ZDY3ZDYwXCIsXCJ1cmxfaWRzXCI6W1wiN2JmOWIyNzlkMjQ5Y2M4ZjBhZmI0NDgzZTMxODAzODk3ODE5Y2Q5YVwiXX0ifQ" style="color:#FFFFFF;">© 2020 EERO LLC SAN FRANCISCO, CA<br>TERMS/PRIVACY<br></a>
+                                                            <a href="http://mail.eero.com/track/click/30727349/eero.com?p=eyJzIjoieFk1MmZRanF6SUpjWjNVNHZJcEl6TVV2MTlvIiwidiI6MSwicCI6IntcInVcIjozMDcyNzM0OSxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL2Vlcm8uY29tXFxcL2xlZ2FsXFxcL3Rvc1wiLFwiaWRcIjpcIjlmZGNhMGM5MTAxMzRjMmU5ODUxZjY0Mzg4ZDY3ZDYwXCIsXCJ1cmxfaWRzXCI6W1wiN2JmOWIyNzlkMjQ5Y2M4ZjBhZmI0NDgzZTMxODAzODk3ODE5Y2Q5YVwiXX0ifQ" style="color:#FFFFFF;">© {{date("Y")}} Biddnest. All rights reserved.</a>
                                                         </td>
                                                     </tr>
                                                 </table>

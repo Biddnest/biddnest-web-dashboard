aa1496675be5bb28efb88ab7493d6bb68a7340e6
imdhanashri <dhanashree.mane78@gmail.com>
2021-08-18 16:11:02 +0530

    testing mail sending

---
+++ resources/views/Email/mail_layout.blade.php
@@ -0,0 +1,66 @@
+<title>order_confirmation</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=455">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,200,600,700" rel="stylesheet" type="text/css">
+
+<style type=”text/css”>
+    td{font-family: Arial, sans-serif !important;}
+
+    a {
+        outline: none;
+        color: #00baf2;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline !important;
+    }
+    .active a:hover {
+        text-decoration: none !important;
+    }
+    .no-link span {
+        text-decoration: none !important;
+        border: none !important;
+        color: #acacac !important;
+    }
+    a[x-apple-data-detectors] {
+        color: inherit !important;
+        text-decoration: none !important;
+    }
+    .active:hover {
+        opacity: 0.8;
+    }
+    .active {
+        -webkit-transition: all 0.3s ease;
+        -moz-transition: all 0.3s ease;
+        -ms-transition: all 0.3s ease;
+        transition: all 0.3s ease;
+    }
+    a img {
+        border: none;
+    }
+    table td {
+        mso-line-height-rule: exactly;
+    }
+    .ExternalClass,
+    .ExternalClass a,
+    .ExternalClass span,
+    .ExternalClass b,
+    .ExternalClass br,
+    .ExternalClass p,
+    .ExternalClass div {
+        line-height: inherit;
+    }
+    ul {
+        Margin: 0 0 0 20px;
+        padding: 0;
+    }
+    .tpl-content {
+        padding: 0 !important;
+    }
+    .cke_show_borders {
+        background: #e4e4e4 !important;
+    }
+    .tpl-repeatmovewrap>.tpl-repeatmove {
+        top: -15px !important;
+    }
+</style>

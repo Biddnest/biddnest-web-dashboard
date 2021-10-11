0d0e8e62b40d79eedcf031c927b1f19661c86e67
imdhanashri <dhanashree.mane78@gmail.com>
2021-10-11 16:43:11 +0530

    fixing multiple vendors

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -346,11 +346,11 @@ class InventoryController extends Controller
             $price_calc->base_price_premium = round($base_price_premium,2);
             $result = $price_calc->save();
 
-            if($result)
-                return true;
-            else
-                return false;
         }
+        if($result)
+            return true;
+        else
+            return false;
 
     }
 

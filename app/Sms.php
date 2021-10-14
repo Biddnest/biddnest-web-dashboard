16875e425d42532f9a70f6014acacd6222761a9e
Amith Raji <amith.raji@diginnovators.com>
2021-10-14 13:01:16 +0530

    Message api key integrated integrated: + Only OTP Delivery fixed - Msg templates pending

--- app/Sms.php
+++ app/Sms.php
@@ -16,7 +16,9 @@ class Sms
             $message = "Your OTP is {$otp}";
             $auth_key = Settings::where("key","msg91_key")->pluck('value')[0];
             $sender_id = Settings::where("key","msg91_sender_id")->pluck('value')[0];
-            $request_url = "http://api.msg91.com/api/sendotp.php?authkey={$auth_key}&mobile=91{$phone}&message=${$message}&sender={$sender_id}&otp={$otp}";
+
+            /* IN THE FOLLOWING SEND OTP ROUTE OF MSG91, COUNTRY CODE OF INDIA (91) IS HARD CODED. CHANGE IF REQUIRED*/
+            $request_url = "http://api.msg91.com/api/sendotp.php?authkey={$auth_key}&mobile=91{$phone}&message={$message}&sender={$sender_id}&otp={$otp}";
             file_get_contents($request_url);
             // Msg91::otp($otp)->to($phone)->send();
             return true;

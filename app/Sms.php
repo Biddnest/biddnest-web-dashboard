bf939a42b96de186e44b5a78d8e3bbe234caa9e4
Amith Raji <amith.raji@diginnovators.com>
2021-10-14 12:49:16 +0530

    Message api key integrated integrated: + Only OTP Delivery fixed - Msg templates pending

--- app/Sms.php
+++ app/Sms.php
@@ -6,22 +6,32 @@ use Craftsys\Msg91\Exceptions\ResponseErrorException;
 use Craftsys\Msg91\Exceptions\ValidationException;
 use Craftsys\Msg91\Facade\Msg91;
 use Exception;
+use App\Models\Settings;
 
 class Sms
 {
     public static function sendOtp($phone, $otp)
     {
-        try {
-            Msg91::otp($otp)->to($phone)->send();
+//        try {
+            $message = "Your OTP is {$otp}";
+            $auth_key = Settings::where("key","msg91_key")->pluck('value')[0];
+            $sender_id = Settings::where("key","msg91_sender_id")->pluck('value')[0];
+            $request_url = "http://api.msg91.com/api/sendotp.php?authkey={$auth_key}&mobile=91{$phone}&message=${$message}&sender={$sender_id}&otp={$otp}";
+            file_get_contents($request_url);
+            // Msg91::otp($otp)->to($phone)->send();
             return true;
-        } catch (ValidationException $e) {
+        /*} catch (ValidationException $e) {
             return [false, "error" => $e->getMessage()];
         } catch (ResponseErrorException $e) {
             return [false, "error" => $e->getMessage()];
         } catch (Exception $e) {
             return [false, "error" => $e->getMessage()];
-        }
+        }*/
 
+    }
+
+    public static function send($phone, $message){
+        return true;
     }
 
 

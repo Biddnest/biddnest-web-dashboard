a86a81f2241b658291e697c50ce9e3f6fc294309
imdhanashri <dhanashree.mane78@gmail.com>
2021-12-22 14:46:35 +0530

    added rolegroup enum

---
+++ app/Enums/RoleGroupEnums.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+
+class RoleGroupEnums{
+    public static $MODUlES = ["dashboard"=>1, "manage_bookings"=>2, "manage_user_roles"=>3, "inventory_management"=>4, "branches"=>5, "vehicle_management"=>6, "vendors_payout"=>7, "service_request"=>8, "reports"=>9];
+}

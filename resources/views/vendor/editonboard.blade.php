e208bf06a07dbdf57de80a890bb61062009fb2ce
imdhanashri <dhanashree.mane78@gmail.com>
2021-12-28 14:30:46 +0530

    added city model API

--- resources/views/vendor/editonboard.blade.php
+++ resources/views/vendor/editonboard.blade.php
@@ -19,15 +19,14 @@
                 </nav>
             </div>
         </div>
-        <div class="d-flex  flex-row text-left ml-120">
+        <div class="d-flex flex-row text-left ml-120">
             <a href="{{route('vendors')}}" class="text-decoration-none">
-                <h3 class="page-subhead text-left p-4 f-20 theme-text">
+                <h3 class="page-subhead text-left f-18" style="margin-top: 10px; !important; color: #2e0789;">
                     <i class="p-1">
                         <img src="{{asset('static/images/Icon feather-chevrons-left.svg')}}" alt="" srcset="">
-                    </i>Back to Vendors
+                    </i> Back to Vendors
                 </h3>
             </a>
-
         </div>
         <!-- Dashboard cards -->
         <div class="d-flex flex-row justify-content-center Dashboard-lcards ">

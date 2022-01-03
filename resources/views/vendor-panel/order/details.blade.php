9cbbcb2062ccbccf3621b38e9254b9356c95d687
imdhanashri <dhanashree.mane78@gmail.com>
2022-01-03 14:56:25 +0530

    bug fixes in vendor panel

--- resources/views/vendor-panel/order/details.blade.php
+++ resources/views/vendor-panel/order/details.blade.php
@@ -72,7 +72,7 @@
                                         <a class="nav-link" id="requirments-tab" href="{{route('vendor.my-quote',['id'=>$booking->public_booking_id])}}">My Quote</a>
                                     </li>--}}
                                     <li class="nav-item">
-                                        <a class="nav-link" id="requirments-tab" href="{{route('vendor.my-bid',['id'=>$booking->public_booking_id])}}">My Bid</a>
+                                        <a class="nav-link disabled" id="requirments-tab" href="#">My Bid</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link disabled" id="requirments-tab" href="#">Schedule</a>

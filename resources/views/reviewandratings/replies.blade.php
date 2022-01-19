2f86843aea51138144bee7dc71edbba78ff98cdf
imdhanashri <dhanashree.mane78@gmail.com>
2022-01-19 18:08:30 +0530

    Unable to edit the bid amount. This action needs to be enabled since customer asking for better price

--- resources/views/reviewandratings/replies.blade.php
+++ resources/views/reviewandratings/replies.blade.php
@@ -216,7 +216,7 @@
                                                         @endif
                                                         @if($tickets->type == \App\Enums\TicketEnums::$TYPE['order_cancellation'])
                                                             <a class="white-text cancel-booking" href="#" data-url="{{route('cancel-order', ['id'=>$ticket_info->public_booking_id])}}">
-                                                                <button class="btn theme-bg white-text w-100">Reject</button>
+                                                                <button class="btn theme-bg white-text w-100">Cancel</button>
                                                             </a>
                                                         @endif
                                                     </div>

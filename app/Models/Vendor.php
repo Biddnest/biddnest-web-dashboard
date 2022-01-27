097588b787bd7066b051875f3b11b1eb0cdc3d32
Amith Raji <amith.raji@diginnovators.com>
2022-01-27 19:57:21 +0530

    Payment Bug Fixed

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Vendor extends Model
 {
     protected $hidden = ['created_at','updated_at','deleted',"password", "pin", "verf_code"];
-    protected $fillable = ["verf_code"];
+//    protected $fillable = ["verf_code"];
     use HasFactory;
 
     public function organization(){
